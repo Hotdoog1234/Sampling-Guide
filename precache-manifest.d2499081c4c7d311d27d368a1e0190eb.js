0cecc08a6de33be137ae9ffb927307e49830efbd
Hotdoog1234 <robert_francis@shieldmw.com>
2024-11-21 08:24:58 -0500

    Updates

---
+++ precache-manifest.d2499081c4c7d311d27d368a1e0190eb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98ac09efe52cac0bfa1c990b325feeb7",
+    "url": "/Sampling-Guide/index.html"
+  },
+  {
+    "revision": "75a5c95dcbb38977d817",
+    "url": "/Sampling-Guide/static/css/main.2a8deeed.chunk.css"
+  },
+  {
+    "revision": "aea54c2cc0e9daa36ce6",
+    "url": "/Sampling-Guide/static/js/2.bbc244fd.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/Sampling-Guide/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "75a5c95dcbb38977d817",
+    "url": "/Sampling-Guide/static/js/main.dc1b8d51.chunk.js"
+  },
+  {
+    "revision": "c680e0e0b0890dababa5",
+    "url": "/Sampling-Guide/static/js/runtime~main.cab10b1d.js"
+  }
+]);
